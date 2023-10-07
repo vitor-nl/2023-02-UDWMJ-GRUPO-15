6f5b0e8df0cdd0f31719f2979218c996b7d83e63
Lucca <emanuellilucca@gmail.comgit config --global user.name Luccagit config --global user.email emanuellilucca@gmail.com>
2023-10-07 18:44:50 -0300

    class socialnetwork

--- socialnetwork.py
+++ socialnetwork.py
@@ -2,9 +2,3 @@ class SocialNetwork:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-
-    def name(self, name):
-        self.name = name
-    
-    def description (self, description):
-        self.description = description
