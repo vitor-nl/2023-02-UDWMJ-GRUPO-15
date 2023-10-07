7d5dc2790ca4bbae388139531ce3a2d44a701392
Srshandler <sr.zebo@live.com>
2023-10-07 17:37:03 -0300

    Corrigit erro de escrita

--- category.py
+++ category.py
@@ -1,6 +1,6 @@
 class Category:
 
-    def _int_(self, id, name, discription):
+    def _int_(self, id, name, description):
         self._id = id
         self._name = name
-        self._discription = discription
+        self._description = description
