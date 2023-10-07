01e63a5863e500b9851384d483f0f3c7128c7c85
Lucca <emanuellilucca@gmail.comgit config --global user.name Luccagit config --global user.email emanuellilucca@gmail.com>
2023-10-07 18:40:10 -0300

    class product

--- product.py
+++ product.py
@@ -2,19 +2,10 @@ import category
 
 class Product(category.Category):
     def __init__(self, name, description, date_fabrication, is_active = True):
-        self._name = name
-        self._description = description
-        self._date_fabrication = date_fabrication
-        self._is_active = is_active
-
-    def name(self, name):
         self.name = name
-    
-    def description(self, description):
         self.description = description
-    
-    def date_fabrication(self, date_fabrication):
-        self._date_fabrication = date_fabrication
+        self.date_fabrication = date_fabrication
+        self._is_active = is_active
 
     def deactivated(self):
         if self._is_active:
